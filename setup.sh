2db489965306d15bbda59f3bde662c5ca3545ba0
Ian Sibley <igsibley980@gmail.com>
2023-04-02 14:40:32 -0700

    Adding more newline goodness

--- setup.sh
+++ setup.sh
@@ -10,12 +10,12 @@ sudo apt-get -y install python3-venv ffmpeg
 
 # create venv if doesn't exist
 if [ ! -d "./venv/" ]; then
-    echo "Installing a virtual environment, since it doesn't exist"
+    echo -e "\nInstalling a virtual environment, since it doesn't exist"
     python -m venv ./venv/
 fi
 
 # make sure all sources are up to date
-echo "Using the virtual environment, and checking installs"
+echo -e "\nUsing the virtual environment, and checking installs"
 source ./venv/bin/activate
 pip install -r requirements.txt
 
