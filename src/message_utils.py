a0c57936dcfd0f38ec6bd46a753f8513c65e7cf9
Ian Sibley <igsibley980@gmail.com>
2023-04-04 22:31:17 -0700

    Adding leave channel option

--- src/message_utils.py
+++ src/message_utils.py
@@ -61,3 +61,44 @@ async def join_channel(client: discord.Client, tchannel: discord.TextChannel, vc
         await tchannel.send("Unable to join your channel! You're not in one?")
 
     return voice_client
+
+async def leave_channels(client: discord.Client) -> None:
+    for vclient in client.voice_clients:
+        if vclient.is_connected():
+            vclient.disconnect()
+
+async def play_sound(client: discord.Client, word: str, tchannel: discord.TextChannel, vchannel: discord.VoiceChannel) -> None:
+            voice_connection = await join_channel(client, tchannel, vchannel)
+
+            if voice_connection and voice_connection.is_connected():
+                soundfile = os.path.join(sound_files_dir, word+".mp3")
+                await tchannel.send(f"Trying to play mp3 matching '{word}'...")
+                
+                soundLoaded = None 
+                if os.path.exists(soundfile):
+                    soundLoaded = discord.FFmpegPCMAudio(soundfile)
+                
+                else:
+                    files = [thing for thing in os.listdir(sound_files_dir)
+                                if word in thing and thing.lower().endswith(".mp3")]
+                    files.sort()
+                    if len(files) == 0:
+                        await tchannel.send(
+                            "I couldn't find any files containing that string? :/")
+                    elif len(files) > 2:
+                        files_str = "  - ".join(files)
+                        await tchannel.send(
+                            f"I found multiple files containing '{word}', "
+                            "could you be more specific?\n"
+                            f"  - {files_str}")
+                    else:
+                        file = files[0]
+                        soundLoaded = discord.FFmpegPCMAudio(os.path.join(sound_files_dir, file))
+                
+                # now play the sound if we found one
+                if soundLoaded:
+                    if voice_connection.is_playing():
+                        voice_connection.stop()
+
+                    voice_connection.play(soundLoaded)
+
--- src/soundboi.py
+++ src/soundboi.py
@@ -5,7 +5,13 @@ import discord
 from dotenv import load_dotenv
 import traceback
 
-from src.message_utils import join_channel, upload_attachments, show_mp3_list
+from src.message_utils import (
+    join_channel,
+    leave_channels,
+    upload_attachments,
+    show_mp3_list,
+    play_sound,
+)
 
 load_dotenv()
 sound_files_dir = os.path.join(os.getcwd(), os.getenv("SOUND_FILE_PATH"))
@@ -48,42 +54,12 @@ async def on_message(message: discord.Message):
         elif words[0].startswith('!join'):
             await join_channel(client, tchannel, vchannel)
         
+        elif words[0].startswith('!leave') or words[0].startswith('!bye'):
+            await leave_channels(client)
+        
         elif words[0].startswith('!play') and len(words) > 1:
-            voice_connection = await join_channel(client, tchannel, vchannel)
-
-            print("Voice connection?")
-            print(f"Connected? {voice_connection.is_connected()}")
-
-            if voice_connection and voice_connection.is_connected():
-                soundfile = os.path.join(sound_files_dir, words[1]+".mp3")
-                
-                soundLoaded = None 
-                if os.path.exists(soundfile):
-                    soundLoaded = discord.FFmpegPCMAudio(soundfile)
-                
-                else:
-                    files = [thing for thing in os.listdir(sound_files_dir)
-                                if words[1] in thing and thing.lower().endswith(".mp3")]
-                    files.sort()
-                    if len(files) == 0:
-                        await tchannel.send(
-                            "I couldn't find any files containing that string? :/")
-                    elif len(files) > 2:
-                        files_str = "  - ".join(files)
-                        await tchannel.send(
-                            f"I found multiple files containing '{words[1]}', "
-                            "could you be more specific?\n"
-                            f"  - {files_str}")
-                    else:
-                        file = files[0]
-                        soundLoaded = discord.FFmpegPCMAudio(os.path.join(sound_files_dir, file))
-                
-                # now play the sound if we found one
-                if soundLoaded:
-                    if voice_connection.is_playing():
-                        voice_connection.stop()
-
-                    voice_connection.play(soundLoaded)
+            await play_sound(client, word, tchannel, vchannel)
+        
     except Exception as e:
         await tchannel.send(f"An error occured... :/\n{e}")
         print(traceback.format_exc())
